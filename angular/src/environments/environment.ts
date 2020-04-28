176ab9a23c70f6d8446a5feb27bc2b97d032dbfe
Marcos Vodánovich <marcosvoda@hotmail.com>
2020-04-28 17:22:06 -0300

    Initial commit

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: false,
+  application: {
+    name: 'Hygge',
+    logoUrl: ''
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44395',
+    clientId: 'Hygge_App',
+    dummyClientSecret: '1q2w3e*',
+    scope: 'Hygge',
+    showDebugInformation: true,
+    oidc: false,
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44331'
+    }
+  },
+  localization: {
+    defaultResourceName: 'Hygge'
+  }
+};
