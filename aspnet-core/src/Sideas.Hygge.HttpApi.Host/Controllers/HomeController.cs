fd49c322b538b430f7d38bb19aa62ba8839aa65f
Marcos Vodánovich <marcosvoda@hotmail.com>
2020-04-29 01:12:25 -0300

    commit

---
+++ aspnet-core/src/Sideas.Hygge.HttpApi.Host/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Sideas.Hygge.Controllers
+{
+    public class HomeController : AbpController
+    {
+        public ActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
